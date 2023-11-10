551ca25f39bd60c251599abb97e2a2c0d9bd2290
acadenasg03 <acadenasg03@educarex.es>
2023-11-10 13:07:35 +0100

    Saludo Persona

---
+++ src/HolaPersona.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HolaPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Dime tu nombre: ");
+		
+		String nombre = s.next();
+
+		System.out.println("Bienvenido " + nombre);
+	}
+
+}
